9ecf1c00c14f165f8ad2798539f230e0a8b7d3fe
GuMarYae <tddanielsjr@gmail.com>
2022-08-20 20:00:08 -0500

    8202022b

--- page313displayingAcolumnOfNumbers.cpp
+++ page313displayingAcolumnOfNumbers.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 int main()
 {
+    int sum = 0;
     int matrix[4][4] =
         {{1, 2, 3, 4},
          {4, 5, 6, 7},
          {8, 9, 10, 11},
          {12, 13, 14, 15}};
-    int sum = 0;
+
     for (int i = 0; i < 4; i++)
-        cout << matrix[i][1] << " ";
+    {
+        sum += matrix[i][1];
+        cout << matrix[i][1] << " "
+             << "\n";
+    }
+    std::cout << "The sum for matrix[1][1] is " << sum << std::endl;
     return 0;
 }
