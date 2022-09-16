2e725d11ec76ad2bbb0a9d0799dcdf400ce61896
GuMarYae <tddanielsjr@gmail.com>
2022-09-16 17:03:09 -0500

    9162022

--- scratchPaper.cpp
+++ scratchPaper.cpp
@@ -1,51 +1,29 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
-#include <ctime>
+#include <cmath>
 #include <ctime>
 #include <string>
 #include <sstream>
-using namespace std;
+#include <vector>
 
-template <typename T>
-T maxValue(T value1, T value2)
-{
-    int result;
-    if (value1 > value2)
-        result = value1;
-    else
-        result = value2;
-    return result;
-}
+/*neither of these compile,.. will get back to this*/
+
 int main()
 {
-    cout << "Maximum between 1 and 3 is "
-         << maxValue(1, 3) << endl;
-   
-   
 
+    std::vector<int> v{1, 9};
+    std::cout << v[1];
 
+    /* std::vector<std::vector<int> > matrix{
+         {1, 2},  // remember, ROWS THEN COLUMNS
+         {3, 4},  // you count down 5 rows
+         {5, 6},  // in eaach row, you go from left to right, 2 columns
+         {7, 8},  // so, to get the 8 its in row 4, 2nd element
+         {9, 0}
+         };       // so, its 3(counting from 0), and 1
+                  // std::cout << matrix[3][1];
+                  */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   
     return 0;
-}
+};
