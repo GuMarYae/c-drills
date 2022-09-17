2a3682babf5e44eb80d8400892399c002d3e37e1
GuMarYae <tddanielsjr@gmail.com>
2022-09-17 10:46:16 -0500

    9172022

--- page492fileInoutAndOutput.cpp
+++ page492fileInoutAndOutput.cpp
@@ -13,19 +13,30 @@ int main()
     std::ofstream output_9162022; // im outputting a variable name called output
 
     output_9162022.open("scores.txt"); // now, im using the variabe name with the open function thats in the frstram header.
-                               // im making a name called scores.. sonce im using ofstream for output, if theres no names called scores.txt
-                               // it will create one. If theres already a name called scores.txt, then itll get over written after
-                               // i use the output.close() function.
+                                       // im making a name called scores.. sonce im using ofstream for output, if theres no names called scores.txt
+                                       // it will create one. If theres already a name called scores.txt, then itll get over written after
+                                       // i use the output.close() function.
+    //you can also write std::ofstream output_9162022('scores.txt)
+    //instead of the two lines above. 
 
     output_9162022 << "Tony"
-           << " "
-           << "D"
-           << " "
-           << "Daniels" <<" "<< 90 << std::endl; // im using output (The variable name I made) as cout
-                                            // the only reason why i cn do that is because its coming from ofstream
-                                            // another type of stream output
+                   << " "
+                   << "D"
+                   << " "
+                   << "Daniels"
+                   << " " << 90 << std::endl; // im using output (The variable name I made) as cout
+                                              // the only reason why i cn do that is because its coming from ofstream
+                                              // another type of stream output
+    output_9162022 << "Shaina"
+                   << " "
+                   << "N"
+                   << " "
+                   << "Daniels"
+                   << " " << 95 << std::endl;
     output_9162022.close();
-    //every time I run this code, whatever is in the code itself will be written to scores.txt.. freakin dope
+    // every time I run this code[output_9162022.close();], whatever is in the code itself will be written to scores.txt.. freakin dope
+
+    std::cout<<"If you see this, we litt, Done"<<std::endl;
 
     return 0;
 }
